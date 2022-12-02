0706ce8b141a1ab3cf6f4d4e35f7e5f29426ee07
Ngô Văn Toàn <toango123zx@gmail.com>
2022-12-02 15:09:55 +0700

    Hoan them get, set, anh, trang thai cua mon an

--- src/model/bean/monan.java
+++ src/model/bean/monan.java
@@ -6,5 +6,56 @@ public class monan {
 	private String TheLoai;
 	private int Gia;
 	private String MoTa;
-	private
+	private String Anh;
+	private String TrangThai;
+	
+	public String getMaMonAn()
+	{
+		return MaMonAn;
+	}
+	public String getTenMonAn()
+	{
+		return TenMonAn;
+	}
+	public String getTheLoai()
+	{
+		return TheLoai;
+	}
+	public int getGia()
+	{
+		return Gia;
+	}
+	public String getAnh()
+	{
+		return Anh;
+	}
+	public String getTrangThai()
+	{
+		return TrangThai;
+	}
+	
+	public void setMaMonAn(String MaMonAn)
+	{
+		this.MaMonAn = MaMonAn;
+	}
+	public void setTenMonAn(String TenMonAn)
+	{
+		this.TenMonAn = TenMonAn;
+	}
+	public void setTheLoai(String TheLoai)
+	{
+		this.TheLoai = TheLoai;
+	}
+	public void setGia(int Gia)
+	{
+		this.Gia = Gia;
+	}
+	public void setAnh(String Anh)
+	{
+		this.Anh = Anh;
+	}
+	public void setTrangThai(String TrangThai)
+	{
+		this.TrangThai = TrangThai;
+	}
 }
